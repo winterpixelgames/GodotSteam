c5c07a9a01586ea1671181be074a6cfeb393c3b4
Jordan Schidlowsky <jordanschidlowsky@gmail.com>
2022-05-27 12:51:21 -0600

    dont build for gamesexpo

--- godotsteam/config.py
+++ godotsteam/config.py
@@ -1,5 +1,6 @@
 def can_build(env, platform):
-	return platform=="x11" or platform=="windows" or platform=="osx"
+	return False
+	#return platform=="x11" or platform=="windows" or platform=="osx"
 
 def configure(env):
 	pass
